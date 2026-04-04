c1f22206c07441777f045923622aff9fa3a47826
Natt <natt.lefevre@gmail.com>
2026-04-04 01:04:53 -0700

    Reorder server and resolve configurations in vite.config.ts for improved clarity

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import viteReact from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    tsconfigPaths: true,
-  },
   plugins: [
     // Order matters
     tanstackStart(),
     viteReact(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  server: {
+    port: 3000,
+  },
 })
