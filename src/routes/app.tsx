f5ec97c01c7eafb257fd743dcc3f61bac8a97504
Natt <natt.lefevre@gmail.com>
2026-04-06 01:38:51 -0700

    Enhance RouteApp component by adding sidebar toggle functionality and updating layout styles for improved responsiveness; change global background color for better visual consistency.

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,15 +1,18 @@
-import { Column, Row } from "@nattstack/ui"
+import { Button, Column, Row, Spacer } from "@nattstack/ui"
 import { createFileRoute, Link, type LinkProps, Outlet } from "@tanstack/react-router"
+import { useState } from "react"
 import { Logo } from "../components/logo"
 
 export const Route = createFileRoute("/app")({
   component: function RouteApp() {
+    const [isSidebarExpanded, setIsSidebarExpanded] = useState(true)
+
     return (
-      <Column className="bg-gray-2 min-h-dvh [--dimension-sidebar-width:240px] [--dimension-topbar-height:56px]">
+      <Column className="min-h-dvh [--dimension-sidebar-width:240px] [--dimension-topbar-height:56px]">
         {/* Topbar */}
         <Row
           as="header"
-          className="bg-gray-2 border-gray-4 sticky top-0 left-0 z-10 h-(--dimension-topbar-height) items-center border-b px-16"
+          className="bg-gray-2 border-gray-4 fixed top-0 left-0 z-10 h-(--dimension-topbar-height) w-full items-center border-b px-16"
         >
           <Link to="/app" className="-ml-8 p-8 transition-opacity hover:opacity-50">
             <Logo />
@@ -21,8 +24,20 @@ export const Route = createFileRoute("/app")({
           {/* Sidebar */}
           <Column
             as="aside"
-            className="border-gray-4 sticky top-(--dimension-topbar-height) left-0 z-10 h-[calc(100dvh-var(--dimension-topbar-height))] w-(--dimension-sidebar-width) shrink-0 overflow-y-auto border-r py-8"
+            className="bg-gray-2 border-gray-4 fixed top-(--dimension-topbar-height) left-0 z-10 h-[calc(100dvh-var(--dimension-topbar-height))] w-(--dimension-sidebar-width) shrink-0 overflow-y-auto border-r py-8 transition-[left] data-[is-sidebar-expanded=false]:-left-(--dimension-sidebar-width)"
+            data-is-sidebar-expanded={isSidebarExpanded}
           >
+            <Column className="mx-16">
+              <Button
+                isFullWidth
+                onClick={() => setIsSidebarExpanded(!isSidebarExpanded)}
+                variant="secondary"
+              >
+                isSidebarExpanded: {isSidebarExpanded.toString()}
+              </Button>
+            </Column>
+            <Spacer height={8} />
+
             {/* Sidebar links */}
             <Column className="gap-y-2 px-8">
               <SidebarLink to="/app">New chat</SidebarLink>
@@ -37,7 +52,11 @@ export const Route = createFileRoute("/app")({
           </Column>
 
           {/* Main */}
-          <Column as="main" className="bg-gray-1 grow px-24 py-24">
+          <Column
+            as="main"
+            className="bg-gray-1 mt-(--dimension-topbar-height) ml-(--dimension-sidebar-width) grow px-16 py-16 transition-[margin-left] data-[is-sidebar-expanded=false]:ml-0"
+            data-is-sidebar-expanded={isSidebarExpanded}
+          >
             <Outlet />
           </Column>
         </Row>
