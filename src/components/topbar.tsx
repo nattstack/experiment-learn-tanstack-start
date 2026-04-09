33dfc848a04b223f56476b3716cf55a3d25641aa
Natt <natt.lefevre@gmail.com>
2026-04-08 18:29:45 -0700

    Add Topbar component for improved layout; update SidebarLink styles for overflow handling and adjust RouteApp styles for better responsiveness

---
+++ src/components/topbar.tsx
@@ -0,0 +1,38 @@
+import { IconLayoutAlignLeft } from "@nattstack/icons-outlined"
+import { Button, Row, Spacer } from "@nattstack/ui"
+import { Link } from "@tanstack/react-router"
+import { Logo } from "./logo"
+import { useSidebarStore } from "./sidebar"
+
+export function Topbar() {
+  const sidebarDesktopIsCollapsed = useSidebarStore((state) => state.desktopIsCollapsed)
+  const setSidebarDesktopIsCollapsed = useSidebarStore((state) => state.desktopSetIsCollapsed)
+
+  return (
+    <>
+      <Row
+        as="header"
+        className="bg-gray-2 fixed top-0 left-0 z-10 h-(--dimension-topbar-height) w-full items-center px-16"
+      >
+        {/* Logo */}
+        <Link to="/app" className="-ml-8 p-8 transition-opacity hover:opacity-50">
+          <Logo />
+        </Link>
+
+        <div className="bg-gray-4 mx-4 h-24 w-1" />
+
+        {/* Toggle sidebar */}
+        <Button
+          className="text-gray-11! hover:text-gray-12! transition-[background-color,color,opacity,translate]!"
+          isIconOnly
+          onClick={() => setSidebarDesktopIsCollapsed(!sidebarDesktopIsCollapsed)}
+          size={32}
+          variant="ghost"
+        >
+          <IconLayoutAlignLeft size={20} />
+        </Button>
+      </Row>
+      <Spacer height="var(--dimension-topbar-height)" />
+    </>
+  )
+}
--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,7 +1,7 @@
-import { Column, Row, Spacer } from "@nattstack/ui"
-import { createFileRoute, Link, Outlet } from "@tanstack/react-router"
-import { Logo } from "../components/logo"
+import { Column, Row } from "@nattstack/ui"
+import { createFileRoute, Outlet } from "@tanstack/react-router"
 import { Sidebar } from "../components/sidebar"
+import { Topbar } from "../components/topbar"
 
 export const Route = createFileRoute("/app")({
   component: function RouteApp() {
@@ -18,10 +18,10 @@ export const Route = createFileRoute("/app")({
           {/* Main */}
           <Column
             as="main"
-            className="bg-gray-1 border-gray-4 rounded-8 shadow-2 mr-8 mb-8 grow border"
+            className="bg-gray-1 border-gray-4 rounded-8 shadow-2 max-768:ml-8 mr-8 mb-8 grow border"
           >
             {/* Main wrapper */}
-            <Column className="mx-auto w-full max-w-768">
+            <Column className="mx-auto w-full max-w-768 p-16">
               <Outlet />
             </Column>
           </Column>
@@ -30,20 +30,3 @@ export const Route = createFileRoute("/app")({
     )
   },
 })
-
-function Topbar() {
-  return (
-    <>
-      <Row
-        as="header"
-        className="bg-gray-2 fixed top-0 left-0 z-10 h-(--dimension-topbar-height) w-full items-center px-16"
-      >
-        {/* Logo */}
-        <Link to="/app" className="-ml-8 p-8 transition-opacity hover:opacity-50">
-          <Logo />
-        </Link>
-      </Row>
-      <Spacer height="var(--dimension-topbar-height)" />
-    </>
-  )
-}
