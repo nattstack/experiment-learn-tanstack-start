05cb26c886e9a474c3a0853e58a6dd0c4fd357e1
Natt <natt.lefevre@gmail.com>
2026-04-09 17:16:19 -0700

    feat(topbar): enhance Topbar layout with new sidebar toggle button and improved structure for mobile responsiveness

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,4 +1,4 @@
-import { IconLayoutAlignLeft } from "@nattstack/icons-outlined"
+import { IconBarsThree, IconLayoutAlignLeft } from "@nattstack/icons-outlined"
 import { Button, Row, Spacer, Tooltip, TooltipContent, TooltipTrigger } from "@nattstack/ui"
 import { Link } from "@tanstack/react-router"
 import { Logo } from "./logo"
@@ -12,35 +12,46 @@ export function Topbar() {
     <>
       <Row
         as="header"
-        className="bg-bg-secondary max-768:border-b max-768:border-border fixed top-0 left-0 z-10 h-(--dimension-topbar-height) w-full items-center px-16"
+        className="bg-bg-secondary max-768:border-b max-768:border-border fixed top-0 left-0 z-10 h-(--dimension-topbar-height) w-full items-center justify-between px-16"
       >
-        {/* Logo */}
-        <Link to="/app" className="-ml-8 p-8 transition-opacity hover:opacity-75">
-          <Logo />
-        </Link>
+        {/* Left */}
+        <Row className="items-center">
+          {/* Logo */}
+          <Link to="/app" className="-ml-8 p-8 transition-opacity hover:opacity-75">
+            <Logo />
+          </Link>
 
-        {/* Separator */}
-        <div className="bg-border max-768:hidden mx-4 h-24 w-1" />
+          {/* Separator */}
+          <div className="bg-border max-768:hidden mx-4 h-24 w-1" />
 
-        {/* Toggle sidebar */}
-        <Tooltip>
-          <TooltipTrigger
-            render={
-              <Button
-                className="text-gray-11! max-768:hidden! hover:text-gray-12! transition-[background-color,color,opacity,translate]!"
-                isIconOnly
-                onClick={() => sidebarDesktopSetIsCollapsed(!sidebarDesktopIsCollapsed)}
-                size={32}
-                variant="ghost"
-              >
-                <IconLayoutAlignLeft size={20} />
-              </Button>
-            }
-          />
-          <TooltipContent className="max-768:hidden!" side="bottom">
-            {sidebarDesktopIsCollapsed ? "Expand" : "Collapse"} sidebar
-          </TooltipContent>
-        </Tooltip>
+          {/* Toggle sidebar desktop */}
+          <Tooltip>
+            <TooltipTrigger
+              render={
+                <Button
+                  className="text-gray-9! max-768:hidden! hover:text-gray-12! transition-[background-color,color,opacity,translate]!"
+                  isIconOnly
+                  onClick={() => sidebarDesktopSetIsCollapsed(!sidebarDesktopIsCollapsed)}
+                  size={32}
+                  variant="ghost"
+                >
+                  <IconLayoutAlignLeft />
+                </Button>
+              }
+            />
+            <TooltipContent className="max-768:hidden!" side="bottom">
+              {sidebarDesktopIsCollapsed ? "Expand" : "Collapse"} sidebar
+            </TooltipContent>
+          </Tooltip>
+        </Row>
+
+        {/* Right */}
+        <Row className="items-center">
+          {/* Toggle sidebar mobile */}
+          <Button className="max-768:flex! -mr-12 hidden!" isIconOnly variant="ghost">
+            <IconBarsThree />
+          </Button>
+        </Row>
       </Row>
       <Spacer height="var(--dimension-topbar-height)" />
     </>
