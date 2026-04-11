be574f2fe9b0a392f596c02b13528e7e3b0ce8de
Natt <natt.lefevre@gmail.com>
2026-04-11 06:24:58 -0700

    refactor(signin): streamline account creation link styling for consistency

--- src/routes/_unauthenticated/signin.tsx
+++ src/routes/_unauthenticated/signin.tsx
@@ -72,10 +72,7 @@ export const Route = createFileRoute("/_unauthenticated/signin")({
           {/* Sign up */}
           <p className="text-14">
             Don't have an account?{" "}
-            <Link
-              className="text-primary-9 font-semibold transition-opacity hover:opacity-75"
-              to="."
-            >
+            <Link className="text-primary-9 font-500 transition-opacity hover:opacity-75" to=".">
               Create one
             </Link>
           </p>
