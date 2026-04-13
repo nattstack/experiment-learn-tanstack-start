4fa7f86b9bbc6a0ad62bae1c416b863116347a30
Natt <natt.lefevre@gmail.com>
2026-04-12 23:14:44 -0700

    refactor(sidebar): remove unused icon and simplify SidebarButtonToggle component rendering

--- src/components/sidebar-button-toggle.tsx
+++ src/components/sidebar-button-toggle.tsx
@@ -1,4 +1,4 @@
-import { IconSidebarHiddenLeftWide, IconSidebarHiddenRightWide } from "@nattstack/icons-outlined"
+import { IconSidebarHiddenLeftWide } from "@nattstack/icons-outlined"
 import { Button, Tooltip, TooltipContent, TooltipTrigger } from "@nattstack/ui"
 import { useSidebarStore } from "./sidebar-content"
 
@@ -16,11 +16,10 @@ export function SidebarButtonToggle() {
             size={36}
             variant="ghost"
           >
-            {sidebarDesktopIsCollapsed ? (
-              <IconSidebarHiddenRightWide className="text-gray-11 group-hover:text-gray-12 transition-colors" />
-            ) : (
-              <IconSidebarHiddenLeftWide className="text-gray-11 group-hover:text-gray-12 transition-colors" />
-            )}
+            <IconSidebarHiddenLeftWide
+              className="text-gray-11 group-hover:text-gray-12 transition-colors [&>path]:last:transition-transform data-[sidebar-desktop-is-collapsed=true]:[&>path]:last:translate-x-10"
+              data-sidebar-desktop-is-collapsed={sidebarDesktopIsCollapsed}
+            />
           </Button>
         }
       />
